ad87c5ae7ef302efeddd99fd28d038f47fc3a874
Knut Valen <knut.valen@icloud.com>
2024-11-01 10:07:29 +0100

    Add StellarApex App

---
+++ StellarApex App/StellarApex App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StellarApex App
+//
+//  Created by Knut Valen on 01/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StellarApex App/StellarApex App/StellarApex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellarApex_AppApp.swift
+//  StellarApex App
+//
+//  Created by Knut Valen on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StellarApex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
